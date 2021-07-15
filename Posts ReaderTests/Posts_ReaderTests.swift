147c0ed4f7f30f0a278ead7b6d13de97946b5fb2
Bogdan <email@server.domen>
2021-07-15 13:40:56 +0300

    Initial Commit

---
+++ Posts ReaderTests/Posts_ReaderTests.swift
@@ -0,0 +1,33 @@
+//
+//  Posts_ReaderTests.swift
+//  Posts ReaderTests
+//
+//  Created by Bogdan Chornobryvets on 15.07.2021.
+//
+
+import XCTest
+@testable import Posts_Reader
+
+class Posts_ReaderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
