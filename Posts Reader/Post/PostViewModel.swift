c92812862f56054ce651f8bb52482dc007629fb6
Bogdan <email@server.domen>
2021-07-19 09:36:35 +0300

    Separated UserViewModel to another file

--- Posts Reader/Post/PostViewModel.swift
+++ Posts Reader/Post/PostViewModel.swift
@@ -60,41 +60,4 @@ class PostDetailsViewModel: Identifiable, PostViewModel, ObservableObject {
     }
 }
 
-class UserViewModel: ObservableObject {
-    var urlString = "https://source.unsplash.com/collection/542909/?sig="
-    let _urlString = "https://source.unsplash.com/collection/542909/?sig="
-    
-    let isEmpty: Bool
-    let uuid = UUID()
-    let id: String
-    let name: String
-    let email: String
-    let address: Address
-    let phone: String
-    let company: Company
-    
-    var imageURL: URL? {
-        URL(string: urlString + id)
-    }
-    
-    init(id: Int) {
-        self.id = "\(id)"
-        name = ""
-        email = ""
-        address = Address(street: "", suite: "", city: "", zipcode: "")
-        phone = ""
-        company = Company(name: "")
-        isEmpty = true
-    }
-    
-    init(user: User) {
-        urlString = _urlString + "\(Int.random(in: 0...100000))" //It allow to update the image each time we do Pull-To-Update on the Details screen
-        id = "\(user.id)"
-        name = user.name
-        email = user.email
-        address = user.address
-        phone = user.phone
-        company = user.company
-        isEmpty = false
-    }
-}
+
---
+++ Posts Reader/Post/UserViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  UserViewModel.swift
+//  Posts Reader
+//
+//  Created by Bogdan Chornobryvets on 19.07.2021.
+//
+
+import Foundation
+
+class UserViewModel: ObservableObject {
+    private var urlString = "https://source.unsplash.com/collection/542909/?sig="
+    private let _urlString = "https://source.unsplash.com/collection/542909/?sig="
+    
+    let isEmpty: Bool
+    let uuid = UUID()
+    let id: String
+    let name: String
+    let email: String
+    let address: Address
+    let phone: String
+    let company: Company
+    
+    var imageURL: URL? {
+        URL(string: urlString + id)
+    }
+    
+    init(id: Int) {
+        self.id = "\(id)"
+        name = ""
+        email = ""
+        address = Address(street: "", suite: "", city: "", zipcode: "")
+        phone = ""
+        company = Company(name: "")
+        isEmpty = true
+    }
+    
+    init(user: User) {
+        urlString = _urlString + "\(Int.random(in: 0...100000))" //It allow to update the image each time we do Pull-To-Update on the Details screen
+        id = "\(user.id)"
+        name = user.name
+        email = user.email
+        address = user.address
+        phone = user.phone
+        company = user.company
+        isEmpty = false
+    }
+}
